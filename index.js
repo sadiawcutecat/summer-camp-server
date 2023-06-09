8f7b2fce78ca663e9864109bab08b28b3cf37b75
sadiawcutecat <98077710+sadiawcutecat@users.noreply.github.com>
2023-06-09 22:54:49 +0600

    .env added

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+require('dotenv').config()
 const port = process.env.PORT || 5000;
 
+//sadiabinte63
+//Godo0Y78VrUIRLes
 
 app.use(cors());
 app.use(express.json());
