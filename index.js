b1164a058a05657cfc56e2af7bb6c9540fc46a4b
sadiawcutecat <98077710+sadiawcutecat@users.noreply.github.com>
2023-06-16 16:56:31 +0600

    added user collection

--- index.js
+++ index.js
@@ -53,93 +53,71 @@ async function run() {
     });
 
     //  Select Class Collection
-    app.post('/api/classes', async (req, res) => {
-      const item = req.body;
-      console.log(item)
-      const result = await classes.insertOne(item);
-      res.send(result);
-    })
+    // app.post('/api/classes', async (req, res) => {
+    //   const item = req.body;
+    //   console.log(item)
+    //   const result = await classes.insertOne(item);
+    //   res.send(result);
+    // })
 
-    app.get('/api/classes', async (req, res) => {
-      const result = await classes.find().toArray();
-      res.send(result);
-    })
+    // app.get('/api/classes', async (req, res) => {
+    //   const result = await classes.find().toArray();
+    //   res.send(result);
+    // })
 
-    app.get('/api/instructors', async (req, res) => {
-      const result = await instructors.find().toArray();
-      res.send(result);
-    })
+    // app.get('/api/instructors', async (req, res) => {
+    //   const result = await instructors.find().toArray();
+    //   res.send(result);
+    // })
 
     //  Select Class Collection
-    app.post('/selectedClasses', async (req, res) => {
-      const item = req.body;
-      console.log(item)
-      const result = await SelectedClasses.insertOne(item);
-      res.send(result);
-    })
-    app.get('/selectedClasses', async (req, res) => {
-      const result = await SelectedClasses.find().toArray();
-      res.send(result);
-    })
+    // app.post('/selectedClasses', async (req, res) => {
+    //   const item = req.body;
+    //   console.log(item)
+    //   const result = await SelectedClasses.insertOne(item);
+    //   res.send(result);
+    // })
+    // app.get('/selectedClasses', async (req, res) => {
+    //   const result = await SelectedClasses.find().toArray();
+    //   res.send(result);
+    // })
 
 
 
     // Manage Classes
-    app.put("/classRequest/:id", async (req, res) => {
-      const id = req.params.id;
-      console.log(id)
-      const updatedRequest = req.body;
-      console.log(updatedRequest)
-      const filter = { _id: new ObjectId(id) };
+    // app.put("/classRequest/:id", async (req, res) => {
+    //   const id = req.params.id;
+    //   console.log(id)
+    //   const updatedRequest = req.body;
+    //   console.log(updatedRequest)
+    //   const filter = { _id: new ObjectId(id) };
 
-      console.log("filter", filter)
-      const options = { upsert: true };
-      const updateDoc = {
-        $set: {
-          chk: updatedRequest.chk,
-        },
-      };
-      const result = await classes.updateOne(
-        filter,
-        updateDoc,
-        options
-      );
-      console.log(result)
-      res.json(result);
-    });
+    //   console.log("filter", filter)
+    //   const options = { upsert: true };
+    //   const updateDoc = {
+    //     $set: {
+    //       chk: updatedRequest.chk,
+    //     },
+    //   };
+    //   const result = await classes.updateOne(
+    //     filter,
+    //     updateDoc,
+    //     options
+    //   );
+    //   console.log(result)
+    //   res.json(result);
+    // });
 
     // manage users
-    app.put("/userRequest/:id", async (req, res) => {
-      const id = req.params.id;
-      console.log(id)
-      const updatedRequest = req.body;
-      console.log(updatedRequest)
-      const filter = { _id: new ObjectId(id) };
-
-      console.log("filter", filter)
-      const options = { upsert: true };
-      const updateDoc = {
-        $set: {
-          category: updatedRequest.category,
-        },
-      };
-      const result = await usersCollection.updateOne(
-        filter,
-        updateDoc,
-        options
-      );
-      console.log(result)
-      res.json(result);
-    });
-
+ 
 
     // Delete
-    app.delete("/classRequest/:id", async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id) };
-      const result = await classes.deleteOne(query);
-      res.json(result);
-    });
+    // app.delete("/classRequest/:id", async (req, res) => {
+    //   const id = req.params.id;
+    //   const query = { _id: new ObjectId(id) };
+    //   const result = await classes.deleteOne(query);
+    //   res.json(result);
+    // });
 
 
 
